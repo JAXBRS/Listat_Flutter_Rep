34d90568d7e82f376c09d009d41ff92bc157a9ef
AlAliSaad <115955949+AlAliSaad@users.noreply.github.com>
2023-12-19 19:41:58 +0300

    Update main.dart
    start with navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:listat/browseList.dart';
+import 'package:listat/controller.dart';
 import 'package:listat/createList.dart';
 import 'LogIn.dart';
 import 'database_test.dart';
+import 'package:listat/myList.dart';
+import 'routes.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: AppPage.getnavbar(),
+    getPages: AppPage.routes
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Shopping List',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyList(),
+//     );
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shopping List',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginPage(),
-      routes: {"createlist": (context) => CreateList()},
-    );
-  }
-}
